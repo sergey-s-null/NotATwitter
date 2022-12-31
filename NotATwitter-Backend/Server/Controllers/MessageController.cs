fd6dbcd6350eff04cf3c439dbdd43b04c38236a8
Sergey Semenov <Laiser399@mail.ru>
2022-12-31 15:26:02 +0300

    Add empty message controller

---
+++ NotATwitter-Backend/Server/Controllers/MessageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class MessageController : ControllerBase
+{
+	[HttpGet]
+	public void Create()
+	{
+		// creation
+	}
+}
