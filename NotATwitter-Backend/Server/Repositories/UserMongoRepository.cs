1f84ac78124246a9288965623887f4bce422ca86
Sergey Semenov <Laiser399@mail.ru>
2022-12-31 19:23:48 +0300

    Add UserMongoRepository

---
+++ NotATwitter-Backend/Server/Repositories/UserMongoRepository.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using Server.Models;
+using Server.Services.Abstract;
+
+namespace Server.Repositories;
+
+public class UserMongoRepository
+{
+	private readonly IMongoDbCollectionsProvider _mongoDbCollectionsProvider;
+
+	public UserMongoRepository(IMongoDbCollectionsProvider mongoDbCollectionsProvider)
+	{
+		_mongoDbCollectionsProvider = mongoDbCollectionsProvider;
+	}
+
+	public async Task<UserModel?> FindByNameAsync(string name)
+	{
+		var userCollection = _mongoDbCollectionsProvider.GetUserCollection();
+
+		var user = await userCollection
+			.Find(x => x.Name == name)
+			.FirstOrDefaultAsync();
+
+		return user;
+	}
+}
