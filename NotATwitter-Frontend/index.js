cb71f49da214b511e8a83bd7cc4f32b0afe89c24
Sergey Semenov <Laiser399@mail.ru>
2023-01-04 02:11:40 +0300

    Hide useless scss import

--- NotATwitter-Frontend/index.js
+++ NotATwitter-Frontend/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import App from "./App.js";
-import "./styles.scss";
+// import "./styles.scss";
 
 const appRouting = (
     <Router>
