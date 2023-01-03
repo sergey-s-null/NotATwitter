613fdef90c54c774402d7bb46a093ab8ba6af296
Sergey Semenov <Laiser399@mail.ru>
2023-01-03 20:20:34 +0300

    Add HardcodedHazelcastConfiguration

---
+++ NotATwitter-Backend/Server/Entities/HardcodedHazelcastConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Server.Entities.Abstract;
+
+namespace Server.Entities;
+
+public class HardcodedHazelcastConfiguration : IHazelcastConfiguration
+{
+	public IReadOnlyList<string> Addresses { get; } = new[]
+	{
+		"127.0.0.1:5701"
+	};
+
+	public string UserLockMapName => "user-lock";
+	public TimeSpan LockCheckPeriod { get; } = TimeSpan.FromMilliseconds(50);
+}
