7106fb390a8e522d5c6614322af77aa2f3a45390
Sergey Semenov <Laiser399@mail.ru>
2023-01-04 02:01:56 +0300

    Add index.js

---
+++ NotATwitter-Frontend/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// todo что не так с Switch?
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import App from "./App.js";
+import "./styles.scss";
+
+const appRouting = (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={App} />
+        </Switch>
+    </Router>
+);
+
+ReactDOM.render(appRouting, document.getElementById("root"));
