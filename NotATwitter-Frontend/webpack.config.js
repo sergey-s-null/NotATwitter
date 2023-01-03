cf0935cc9fc9285615b22f4da83112c8fbde36e0
Sergey Semenov <Laiser399@mail.ru>
2023-01-04 01:54:39 +0300

    Add webpack configuration

---
+++ NotATwitter-Frontend/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const webpack = require("webpack");// todo проверить, необходимо ли, или можно удалить
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve("dist"),
+        publicPath: "/",
+    },
+    module: {
+        rules:[
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            },
+            {
+                test: /\.html$/,
+                use: "html-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "index.html"
+        }),
+    ]
+}
