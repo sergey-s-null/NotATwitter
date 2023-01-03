7152fc4bf5b2cd4cef05214ef9ec864b5475131d
Sergey Semenov <Laiser399@mail.ru>
2023-01-03 20:20:13 +0300

    Add realisation of Hazelcast client provider

--- NotATwitter-Backend/Server/Entities/Abstract/IHazelcastConfiguration.cs
+++ NotATwitter-Backend/Server/Entities/Abstract/IHazelcastConfiguration.cs
@@ -2,6 +2,8 @@
 
 public interface IHazelcastConfiguration
 {
+	IReadOnlyList<string> Addresses { get; }
+
 	string UserLockMapName { get; }
 	TimeSpan LockCheckPeriod { get; }
 }
---
+++ NotATwitter-Backend/Server/Services/HazelcastClientProvider.cs
@@ -0,0 +1,43 @@
+﻿using Hazelcast;
+using Server.Entities.Abstract;
+using Server.Services.Abstract;
+
+namespace Server.Services;
+
+public sealed class HazelcastClientProvider : IHazelcastClientProvider, IAsyncDisposable
+{
+	public IHazelcastClient Client => _client ??= CreateClientAsync().Result;
+
+	private readonly IHazelcastConfiguration _hazelcastConfiguration;
+
+	private IHazelcastClient? _client;
+
+	public HazelcastClientProvider(IHazelcastConfiguration hazelcastConfiguration)
+	{
+		_hazelcastConfiguration = hazelcastConfiguration;
+	}
+
+	private async Task<IHazelcastClient> CreateClientAsync()
+	{
+		var options = new HazelcastOptionsBuilder()
+			.With(x =>
+			{
+				foreach (var address in _hazelcastConfiguration.Addresses)
+				{
+					x.Networking.Addresses.Add(address);
+				}
+			})
+			.Build();
+
+		return await HazelcastClientFactory.StartNewClientAsync(options);
+	}
+
+	public async ValueTask DisposeAsync()
+	{
+		if (_client is not null)
+		{
+			await _client.DisposeAsync();
+			_client = null;
+		}
+	}
+}
