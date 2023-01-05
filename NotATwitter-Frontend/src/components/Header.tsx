ac70486e2b9c5d6ca03712fc8a570c7908dbaf57
Sergey Semenov <Laiser399@mail.ru>
2023-01-06 01:11:13 +0300

    Add elements for authorized user in Header

--- NotATwitter-Frontend/src/components/Header.tsx
+++ NotATwitter-Frontend/src/components/Header.tsx
@@ -1,8 +1,10 @@
-import { Button, Col, Container, Navbar } from "react-bootstrap";
+import { Button, Col, Container, InputGroup, Navbar } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import * as React from "react";
 
 const Header = () => {
+    const authorized = true;
+
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
@@ -14,29 +16,71 @@ const Header = () => {
                     </LinkContainer>
                 </Col>
 
-                <Col sm="auto">
-                    <LinkContainer to="authorization">
+                {authorized &&
+                    <Col sm="auto" className="me-3">
                         <Button
-                            variant="secondary"
+                            variant="success"
                             size="sm"
-                            className="mx-1"
+                            style={{ padding: "0px 6px" }}
                         >
-                            Sign in
+                            <i
+                                className="bi bi-plus"
+                                style={{ color: "white", fontSize: "20px" }}
+                            />
                         </Button>
-                    </LinkContainer>
-                </Col>
+                    </Col>
+                }
 
-                <Col sm="auto">
-                    <LinkContainer to="registration">
-                        <Button
-                            variant="primary"
-                            size="sm"
-                            className="mx-1"
-                        >
-                            Sign up
-                        </Button>
-                    </LinkContainer>
-                </Col>
+                {authorized &&
+                    <Col sm="auto">
+                        <InputGroup>
+                            <Button
+                                variant="outline-light"
+                                size="sm"
+                            >
+                                Some user
+                            </Button>
+                            <Button
+                                variant="light"
+                                size="sm"
+                                style={{ padding: "0px 6px" }}
+                            >
+                                <i
+                                    className="bi bi-box-arrow-right"
+                                    style={{ fontSize: "20px" }}
+                                />
+                            </Button>
+                        </InputGroup>
+                    </Col>
+                }
+
+                {!authorized &&
+                    <Col sm="auto">
+                        <LinkContainer to="authorization">
+                            <Button
+                                variant="secondary"
+                                size="sm"
+                                className="mx-1"
+                            >
+                                Sign in
+                            </Button>
+                        </LinkContainer>
+                    </Col>
+                }
+
+                {!authorized &&
+                    <Col sm="auto">
+                        <LinkContainer to="registration">
+                            <Button
+                                variant="primary"
+                                size="sm"
+                                className="mx-1"
+                            >
+                                Sign up
+                            </Button>
+                        </LinkContainer>
+                    </Col>
+                }
             </Container>
         </Navbar>
     );
