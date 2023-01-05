9a3007c9e151b1bdf15720de468182ba3d66cfb1
Sergey Semenov <Laiser399@mail.ru>
2023-01-05 23:18:16 +0300

    Add empty messages in App; Set margin for SearchForm

--- NotATwitter-Frontend/src/App.tsx
+++ NotATwitter-Frontend/src/App.tsx
@@ -2,13 +2,18 @@ import * as React from "react";
 import { Container } from "react-bootstrap";
 import SearchForm from "./SearchForm";
 import Header from "./Header";
+import Message from "./Message";
 
 export default function App() {
     return (
         <div>
             <Header/>
             <Container>
-                <SearchForm/>
+                <SearchForm className={"mt-2 mb-5"}/>
+
+                {Array.from({ length: 100 }).map(() => {
+                    return <Message className="my-3"/>;
+                })}
             </Container>
         </div>
     );
