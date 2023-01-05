b0ddce55ca6d07c7b518b6163fd714a176942a08
Sergey Semenov <Laiser399@mail.ru>
2023-01-05 11:55:55 +0300

    Add simple router

--- NotATwitter-Frontend/src/index.tsx
+++ NotATwitter-Frontend/src/index.tsx
@@ -1,7 +1,19 @@
 import * as React from "react";
-
-import App from "./App";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>
+    }
+]);
+
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App/>);
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
+);
