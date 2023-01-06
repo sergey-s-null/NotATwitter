08b4ca6e9d6a32150e3702bb7a4f8eb03e2204d4
Sergey Semenov <Laiser399@mail.ru>
2023-01-06 17:50:04 +0300

    Add TestController

---
+++ NotATwitter-Backend/Server/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestController : ControllerBase
+{
+	public Task<ActionResult> Get()
+	{
+		return Task.FromResult<ActionResult>(Ok("Hi, I'm working!"));
+	}
+}
