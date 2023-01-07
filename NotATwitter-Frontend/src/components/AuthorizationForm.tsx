5b425fe405f3c0e17fa367a5a0ddaa55c2a479af
Sergey Semenov <Laiser399@mail.ru>
2023-01-07 19:43:28 +0300

    Change using controls in AuthorizationForm

--- NotATwitter-Frontend/src/components/AuthorizationForm.tsx
+++ NotATwitter-Frontend/src/components/AuthorizationForm.tsx
@@ -1,20 +1,22 @@
 import * as React from "react";
-import { Button, Card, FloatingLabel, Form } from "react-bootstrap";
+import { Button, Card, FloatingLabel, FormControl } from "react-bootstrap";
+import { Form as RouterForm } from "react-router-dom";
+
 
 const AuthorizationForm = () => {
     return (
         <Card className="mx-auto mt-5" style={{ maxWidth: "400px" }}>
             <Card.Body>
-                <Form>
+                <RouterForm method="post">
                     <h3 className="text-center mb-3">Authorization</h3>
-                    <Form.FloatingLabel label="Name" className="mb-3">
-                        <Form.Control type="input" placeholder="Name" size="sm"/>
-                    </Form.FloatingLabel>
-                    <FloatingLabel label="Password" className="mb-3">
-                        <Form.Control type="password" placeholder="Password"/>
+                    <FloatingLabel label="Name" className="mb-3">
+                        <FormControl type="input" placeholder="Name" size="sm"/>
                     </FloatingLabel>
-                    <Button className="d-block mx-auto">Sing in</Button>
-                </Form>
+                    <FloatingLabel label="Password" className="mb-3">
+                        <FormControl type="password" placeholder="Password"/>
+                    </FloatingLabel>
+                    <Button type="submit" className="d-block mx-auto">Sing in</Button>
+                </RouterForm>
             </Card.Body>
         </Card>
     );
